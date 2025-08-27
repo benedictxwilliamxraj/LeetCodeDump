5fc6afceccaaa13efcdbdd69f677170a7690477f
benedictraj <benedictwraj117@gmail.com>
2025-08-27 11:13:17 -0400

    max depth

---
+++ Tree/MaxDepth.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        #bfs
+        if root is None:
+            return 0
+        q = deque()
+        q.append(root)
+        cnt = 0
+        while q:
+            cnt+=1
+            for i in range(len(q)):
+                node = q.popleft()
+                if node.left is not None:
+                    q.append(node.left)
+                if node.right is not None:
+                    q.append(node.right)
+        
+        return cnt
